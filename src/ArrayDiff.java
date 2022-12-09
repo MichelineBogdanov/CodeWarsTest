d52107aa7b1834839d875a99af791ef42733d513
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2022-12-10 00:41:54 +0300

    ArrayDiffV2

--- src/ArrayDiff.java
+++ src/ArrayDiff.java
@@ -18,35 +18,41 @@ public class ArrayDiff {
         System.out.println(Arrays.toString(arrayDiff(new int[]{1, 2, 2}, new int[]{1})));
         System.out.println(Arrays.toString(arrayDiff(new int[]{1, 2, 2}, new int[]{2})));
         System.out.println(Arrays.toString(arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2})));
+        System.out.println(Arrays.toString(arrayDiff(new int[] {13, 22, 342, 2, 3}, new int[] {2})));
         System.out.println("--------------------");
         System.out.println(Arrays.toString(arrayDiff2(new int[]{1, 2}, new int[]{1})));
         System.out.println(Arrays.toString(arrayDiff2(new int[]{1, 2, 2}, new int[]{1})));
         System.out.println(Arrays.toString(arrayDiff2(new int[]{1, 2, 2}, new int[]{2})));
+        System.out.println(Arrays.toString(arrayDiff2(new int[] {1, 2, 2, 2, 3}, new int[] {2})));
         System.out.println(Arrays.toString(arrayDiff2(new int[] {13, 22, 342, 2, 3}, new int[] {2})));
     }
 
     public static int[] arrayDiff(int[] a, int[] b) {
-        ArrayList<Integer> collect1 = Arrays.stream(a)
+        ArrayList<Integer> init = Arrays.stream(a)
                 .boxed()
                 .collect(Collectors.toCollection(ArrayList::new));
-        ArrayList<Integer> collect2 = Arrays.stream(b)
+        ArrayList<Integer> check = Arrays.stream(b)
                 .boxed()
                 .collect(Collectors.toCollection(ArrayList::new));
-        collect1.removeAll(collect2);
-        return collect1.stream().mapToInt(i -> i).toArray();
+        init.removeAll(check);
+        return init.stream().mapToInt(i -> i).toArray();
     }
 
 
     //не работает!
     public static int[] arrayDiff2(int[] a, int[] b) {
-        String string = "";
-        for (int i : a) {
-            string = string.concat(String.valueOf(i));
+        //String init = Arrays.toString(a).replace("[", "").replace("]", " ").replaceAll(", ", " ");
+        String init = Arrays.stream(a)
+                .mapToObj(i -> i + " ")
+                .collect(Collectors.joining());
+        System.out.println(init);
+        ArrayList<String> collect = Arrays.stream(b)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toCollection(ArrayList::new));
+        for (String s : collect) {
+            init = init.replaceAll("%s ".formatted(s), "");
         }
-        for (int i : b) {
-            string = string.replace(String.valueOf(i), "");
-        }
-        return string.chars().map(i -> Character.digit(i, 10)).toArray();
+        return Arrays.stream(init.split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 
 }
